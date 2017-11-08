7ca595c2fac2a792bab55d3cb222521159b92064
Nathan <nathanmolle25@gmail.com>
2017-11-08 16:39:09 +0100

    When we create an user, we make sure that he is related to a known session.

--- common/models/user.js
+++ common/models/user.js
@@ -1,5 +1,24 @@
 'use strict';
 
 module.exports = function(User) {
+    var app = require('../../server/server');
 
+    
+    User.observe('before save', function(ctx,next) {
+        var Session = app.models.Session;
+        var register_session_id=ctx.instance.registerSessionId;
+        Session.findById(register_session_id, function(err, session) {
+            if(session===null){
+                var error = new Error();
+                error.status = 400;
+                error.message = "The session doesn't exist"
+               next(error);
+            }else{
+                next();                
+            }
+
+         });
+         
+       
+      });
 };
